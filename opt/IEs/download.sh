2f251182e8b73f43b219ffdfdfa38d87fc9265de
Sune Kirkeby <mig@ibofobi.dk>
2012-06-15 16:30:03 +0200

    Sane version of ievms.

---
+++ opt/IEs/download.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+for version in "$@"
+do
+    case ${version} in
+        8)
+            url="http://download.microsoft.com/download/B/7/2/B72085AE-0F04-4C6F-9182-BF1EE90F5273/Windows_7_IE9.part0{1.exe,2.rar,3.rar,4.rar,5.rar,6.rar,7.rar}"
+        ;;
+
+        9)
+            url="http://download.microsoft.com/download/B/7/2/B72085AE-0F04-4C6F-9182-BF1EE90F5273/Windows_7_IE8.part0{1.exe,2.rar,3.rar,4.rar}"
+        ;;
+
+        *)
+            echo "Unknown IE version: ${version}"
+            exit 1
+            ;;
+    esac
+
+    wget ${url}
+done
---
+++ opt/IEs/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -e
+
+ga_iso="/usr/share/virtualbox/VBoxGuestAdditions.iso"
+
+for version in "$@"
+do
+    vm=IE${version}
+    vm_type=Windows7
+    vm_path="$HOME/VirtualBox VMs/${vm}"
+    archive="Windows_7_${vm}.part01.exe"
+
+    case ${version} in
+        8) vhd="Windows 7.vhd" ;;
+        9) vhd="Win7_IE8.vhd" ;;
+        *)
+            echo "Unknown IE version: ${version}"
+            exit 1
+            ;;
+    esac
+
+    mkdir -p "${vm_path}/${vhd}"
+    cd "${vm_path}/${vhd}"
+    unrar e "${OLDPWD}/${archive}"
+
+    VBoxManage createvm --name "${vm}" --ostype "${vm_type}" --register
+    VBoxManage modifyvm "${vm}" --memory 1024 --vram 64
+    VBoxManage storagectl "${vm}" --name "IDE Controller" \
+                --add ide --controller PIIX4 --bootable on
+    VBoxManage internalcommands sethduuid "${vm_path}/${vhd}"
+    VBoxManage storageattach "${vm}" --storagectl "IDE Controller" \
+                --port 0 --device 0 --type hdd --medium "${vm_path}/${vhd}"
+    VBoxManage storageattach "${vm}" --storagectl "IDE Controller" \
+                --port 0 --device 1 --type dvddrive --medium "${ga_iso}"
+    VBoxManage snapshot "${vm}" take clean \
+                --description "The initial VM state"
+done
