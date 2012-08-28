9c6350f3f200d3a15a6882301e52bc7744e18771
Sune Kirkeby <mig@ibofobi.dk>
2012-08-28 08:57:04 +0200

    Download link for IE 10 preview.

--- opt/IEs/download.sh
+++ opt/IEs/download.sh
@@ -13,6 +13,10 @@ do
             url="http://download.microsoft.com/download/B/7/2/B72085AE-0F04-4C6F-9182-BF1EE90F5273/Windows_7_IE8.part0{1.exe,2.rar,3.rar,4.rar}"
         ;;
 
+        10)
+            wget -O 'Windows_8_IE10.iso' 'http://go.microsoft.com/fwlink/?LinkId=251533'
+        ;;
+
         *)
             echo "Unknown IE version: ${version}"
             exit 1
