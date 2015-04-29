5985dd796771d8dc0dda017c02d7246c9f2f296c
Sune Kirkeby <ski@csis.dk>
2015-04-29 12:21:23 +0200

    Stop NetworkManager screwing with dnsmasq.

--- opt/puppet/modules/dnsmasq/manifests/init.pp
+++ opt/puppet/modules/dnsmasq/manifests/init.pp
@@ -9,4 +9,12 @@ class dnsmasq {
         source => 'puppet:///modules/dnsmasq/dhclient.conf',
         owner => 'root', group => 'root', mode => 0644
     }
+    file { '/etc/default/dnsmasq':
+        source => 'puppet:///modules/dnsmasq/default',
+        owner => 'root', group => 'root', mode => 0644
+    }
+    file { '/etc/dbus-1/system.d/dnsmasq-local.conf':
+        source => 'puppet:///modules/dnsmasq/dnsmasq-local.conf',
+        owner => 'root', group => 'root', mode => 0644
+    }
 }
