7eb9037fbe1ee8efc28ad56318a35fb46bdebf34
Sune Kirkeby <mig@ibofobi.dk>
2017-01-11 10:01:14 +0100

    Fix syntax snafu.

--- opt/puppet/modules/desktop/manifests/init.pp
+++ opt/puppet/modules/desktop/manifests/init.pp
@@ -21,7 +21,7 @@ class desktop {
 
     ### Packages I do not want.
     package { [
-            'puppet' 'gnome-user-share', 'mate-user-share',
+            'puppet', 'gnome-user-share', 'mate-user-share',
         ]:
             ensure => absent,
     }
